b14943ae6eeea510ae8ebb24bdffb3e37c063f17
Henrique Mariz <henriquemariz18@gmail.com>
2021-12-19 17:36:50 -0300

    Temp boss anim

---
+++ Assets/SkullJawAnimation.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class SkullJawAnimation : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform skullJaw;
+
+    [SerializeField] 
+    private Vector3 endRotation;
+
+    [SerializeField] 
+    private float animDuration;
+
+    [SerializeField] 
+    private RotateMode animRotateMode;
+    
+    private Sequence _sequence;
+    private Vector3 _initRotation;
+    void Start()
+    {
+        _initRotation = skullJaw.transform.localRotation.eulerAngles;
+        _sequence = DOTween.Sequence();
+        _sequence.Append(skullJaw.transform.DOLocalRotate(endRotation, animDuration, animRotateMode));
+        _sequence.Append(skullJaw.transform.DOLocalRotate(_initRotation, animDuration, animRotateMode));
+        _sequence.SetLoops(-1);
+        _sequence.Play();
+    }
+}
