4ff027831fe124c0d7ccbbb01df05310d4344197
riqmariz <ham2@cin.ufpe.br>
2021-12-19 12:35:36 -0300

    Boss add movement speed bonus on hit

---
+++ Assets/Game/Scripts/Enemy/BossAddMovementOnHit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Event = SharedData.Events.Event;
+
+public class BossAddMovementOnHit : MonoBehaviour
+{
+    [SerializeField] 
+    private Event onValidHitOnBoss;
+
+    [SerializeField] 
+    private MovementComponent movementComponent;
+
+    [SerializeField] 
+    private float bonusMovementOnHit;
+
+    private int index;
+
+    private void Awake()
+    {
+        onValidHitOnBoss.AddCallback(AddMovementOnHitEvent);
+    }
+
+    private void OnDestroy()
+    {
+        onValidHitOnBoss.RemoveCallback(AddMovementOnHitEvent);
+    }
+    private void AddMovementOnHitEvent()
+    {
+        var currentSpeed = movementComponent.GetSpeed();
+        var bonusSpeed = bonusMovementOnHit;
+        movementComponent.SetSpeed(currentSpeed+bonusSpeed);
+        Debug.Log("Adding "+bonusMovementOnHit+" movement bonus on boss");
+    }
+    
+}
