6723a624035df9d368db7565d0fce16fe3866beb
Henrique Mariz <henriquemariz18@gmail.com>
2021-12-20 20:57:17 -0300

    flow of Main menu + loading + Level select

---
+++ Assets/Game/Scripts/ScreenshotManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.IO;
+using System;
+
+public class ScreenshotManager : MonoBehaviour
+{
+    private string _caminho;
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        _caminho = Application.persistentDataPath + "/Capturas/";
+
+        if (!Directory.Exists(_caminho))
+        {
+            Directory.CreateDirectory(_caminho);
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Print))
+        {
+            string nomeImagem = _caminho + "captura_"+DateTime.Now.Ticks.ToString() + ".png";
+            ScreenCapture.CaptureScreenshot(nomeImagem,2);
+            Debug.Log("screenshot taken on: "+_caminho);
+        }
+    }
+}
