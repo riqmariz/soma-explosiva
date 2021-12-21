270a2ba77beed511ca3d759f6f93fd471926e178
Matheus <mbmt@cin.ufpe.br>
2021-12-20 23:08:08 -0300

    add game over and complete popup

---
+++ Assets/Scripts/Popups/GameOverPopup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverPopup : GenericPopup
+{
+    public void RequestShow() => PopupManager.GetInstance().ShowPopup<GameOverPopup>();
+    public override void RequestHide() => PopupManager.GetInstance().ClosePopup<GameOverPopup>();
+
+    public void Home() => GameManager.GetInstance().Home();
+    public void Reset() => GameManager.GetInstance().Reset();
+}
