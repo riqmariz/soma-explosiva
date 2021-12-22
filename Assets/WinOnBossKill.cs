8b9e23a03426267760a8a590a2d436ecb887566d
Henrique Mariz <henriquemariz18@gmail.com>
2021-12-22 18:50:03 -0300

    adding a condition to win on boss level

---
+++ Assets/WinOnBossKill.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Event = SharedData.Events.Event;
+
+public class WinOnBossKill : MonoBehaviour
+{
+    [SerializeField] 
+    private Event onBossKill;
+
+    private void Awake()
+    {
+        onBossKill.AddCallback(LevelComplete);
+    }
+
+    private void OnDestroy()
+    {
+        onBossKill.RemoveCallback(LevelComplete);
+    }
+
+    private void LevelComplete()
+    {
+        PopupManager.GetInstance().ShowPopup<GameCompletePopup>();
+    }
+}
